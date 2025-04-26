525e6a2f3d66e17abaa037ab882c8fb5de78e35b
eScheurer <enya.scheurer@quickline.ch>
2025-04-26 15:31:08 +0200

    makes the thread pool shrink dynamically

--- mainPool.c
+++ mainPool.c
@@ -7,10 +7,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <time.h>
+#include <errno.h>
 
 #define MAX_QUEUE 128 //reicht das aus? sollte queue dynamsich wachsen?
 #define INITIAL_THREADS 4
 #define MAX_THREADS 128 //sinvoll?
+#define THREAD_IDLE_TIMEOUT 5 // (Sekunden), sinvoll?
 
 /** Struct for each Chunk of the queue */
 typedef struct Task {
@@ -18,12 +21,6 @@ typedef struct Task {
     char task_name[64]; //for Buffer
     //TODO: how large should buffer be?
 } Task;
-
-//typedef struct Task_queue {
-  //  Task task; //info what to do
-    //int count; // ?????
-    //int read_pos; //pointer to where to read out the next task
-//} Task_queue;
 
 static Task task_queue[MAX_QUEUE];
 static int queue_front = 0, queue_rear =0, queue_count = 0;
@@ -34,10 +31,27 @@ static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 static pthread_t *threads;
 static int thread_count;
 static bool keep_running = true;
-static int active_threads, idle_threads;
+static int active_threads = 0, idle_threads = 0;
 
 //Funktionsprototyp
 void add_thread_to_pool();
+void remove_thread_from_pool();
+
+// struct for timespec
+struct timespec make_timeout_timespec(int seconds) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    ts.tv_sec += seconds;
+    return ts;
+}
+
+/**
+ * @return 0 if Task available, ETIMEOUT if time out is reached
+ */
+int wait_for_task_with_timeout(pthread_cond_t *cond, pthread_mutex_t *lock, int seconds) {
+    struct timespec ts = make_timeout_timespec(seconds);
+    return pthread_cond_timedwait(cond, lock, &ts);
+}
 
 /**
  * Worker thread function to handle the task (client socket)
@@ -49,33 +63,42 @@ void* thread_worker(void* arg){
         // Wait for tasks from client socket
         pthread_mutex_lock(&lock);
         idle_threads ++;
-        while (queue_count == 0 && keep_running) {
-            pthread_cond_wait(&cond, &lock);
-        }
+
+        int resume = wait_for_task_with_timeout(&cond, &lock, THREAD_IDLE_TIMEOUT);
+
 
         if (!keep_running) {
             idle_threads --;
             pthread_mutex_unlock(&lock);
             break;
         }
+        if (resume == ETIMEDOUT && thread_count > INITIAL_THREADS) {
+            idle_threads--;
+            remove_thread_from_pool();
+        }
 
-        task = task_queue[queue_front];
-        queue_front = (queue_front + 1) % MAX_QUEUE;
-        queue_count--;
+        if (resume == 0) {
+            task = task_queue[queue_front];
+            queue_front = (queue_front + 1) % MAX_QUEUE;
+            queue_count--;
 
-        idle_threads --;
-        active_threads ++;
+            idle_threads --;
+            active_threads ++;
 
-        pthread_mutex_unlock(&lock);
+            pthread_mutex_unlock(&lock);
 
-        // Call send_time to handle client communication
-        extern void handle_request(int client_socket);
-        handle_request(task.socket_id);
-        // hier verschiedene taskts abarbeiten
+            // Call send_time to handle client communication
+            extern void handle_request(int client_socket);
+            handle_request(task.socket_id);
+            // hier verschiedene taskts abarbeiten
 
-        pthread_mutex_lock(&lock);
-        active_threads--;
-        pthread_mutex_unlock(&lock);
+            pthread_mutex_lock(&lock);
+            active_threads--;
+            pthread_mutex_unlock(&lock);
+        }
+        else {
+            pthread_mutex_unlock(&lock);
+        }
     }
     return NULL;
 }
@@ -126,7 +149,21 @@ void init_thread_pool(){
 }
 
 void add_thread_to_pool() {
-    threads = realloc(threads, sizeof(pthread_t) * (thread_count + 1)); //TODO: is realloc critical?
-    pthread_create(&threads[thread_count], NULL, thread_worker, NULL);
-    thread_count ++;
+    pthread_t *new_threads = realloc(threads, sizeof(pthread_t) * (thread_count + 1)); //first realloc to new place instead of overwriting to avoid losing memory leak in case of error
+    if (new_threads == NULL) {
+        printf("Error: Failed to allocate memory for new thread\n");
+        return;
+        }
+        threads = new_threads;
+
+        pthread_create(&threads[thread_count], NULL, thread_worker, NULL);
+        thread_count++;
+}
+
+// Should only be called by worker threads that want to terminate themselves, not by any others, cause it will kill your thread
+void remove_thread_from_pool() {
+    thread_count --;
+    idle_threads --;
+    pthread_mutex_unlock(&lock);
+    pthread_exit(NULL);
 }
